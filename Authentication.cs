cbd57620460b9f7655c005773137aa7cba4b4779
Avner Shahar-Kashtan <avner@strawjackal.org>
2018-07-14 20:46:10 +0300

    initial

---
+++ Authentication.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace VSTSMonitor
+{
+    internal class Authentication
+    {
+        internal const string VSTSResourceId = "499b84ac-1321-427f-aa17-267ca6975798";
+        internal const string clientId = "872cd9fa-d31f-45e0-9eab-6e460a02d1f1";          //change to your app registration's Application ID, unless you are an MSA backed account
+        internal const string replyUri = "urn:ietf:wg:oauth:2.0:oob";         
+
+        public static async Task<string> GetAccessToken()
+        {
+            AuthenticationContext ctx = GetAuthenticationContext(null);
+            var result = await ctx.AcquireTokenAsync(VSTSResourceId, clientId, new Uri(replyUri), new PlatformParameters(PromptBehavior.Auto));
+            return result.AccessToken;
+        }
+
+        private static AuthenticationContext GetAuthenticationContext(string tenant)
+        {
+            AuthenticationContext ctx = null;
+            if (tenant != null)
+                ctx = new AuthenticationContext("https://login.microsoftonline.com/" + tenant);
+            else
+            {
+                ctx = new AuthenticationContext("https://login.windows.net/common");
+                if (ctx.TokenCache.Count > 0)
+                {
+                    string homeTenant = ctx.TokenCache.ReadItems().First().TenantId;
+                    ctx = new AuthenticationContext("https://login.microsoftonline.com/" + homeTenant);
+                }
+            }
+
+            return ctx;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Speech.Synthesis;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
+using VSTS.Net;
+using VSTS.Net.Models.PullRequests;
+using VSTS.Net.Models.Request;
+using Windows.Data.Xml.Dom;
+
+namespace VSTSMonitor
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Debugger.IsAttached)
+                AllocConsole();
+
+            if (args.FirstOrDefault() == "all") 
+                LastChecked = DateTime.MinValue;
+            
+            var execution = Task.Run(ConnectAndMonitor);
+            execution.Wait();
+
+        }
+
+        private static string Instance => ConfigurationManager.AppSettings["instance"];
+        private static string Project => ConfigurationManager.AppSettings["project"];
+
+        private static DateTime LastChecked
+        {
+            get => VSTSMonitor.Default.LastChecked;
+            set
+            {
+                VSTSMonitor.Default.LastChecked = value;
+                VSTSMonitor.Default.Save();
+            }
+        }
+
+        static async Task ConnectAndMonitor()
+        {
+            
+            while (true)
+            {
+                var client = VstsClient.Get(Instance, await Authentication.GetAccessToken());
+                await client.MonitorPullRequests(LastChecked);
+
+                LastChecked = DateTime.UtcNow;
+                
+                await Task.Delay(TimeSpan.FromSeconds(30));
+            }
+            
+
+
+        }
+
+        public static async Task MonitorPullRequests(this VstsClient client, DateTime lastChecked)
+        {
+            var newPullRequests = await client.GetPullRequestsAsync(Project, Project, new PullRequestQuery
+            {
+                CreatedAfter = lastChecked
+            });
+
+            Notify(newPullRequests);
+
+         
+        }
+
+        private static ToastNotifier _notifier = ToastNotificationManager.CreateToastNotifier("0");
+        private static readonly SpeechSynthesizer _speechSynthesizer = new System.Speech.Synthesis.SpeechSynthesizer();
+
+        private const string NotificationTemplate = "<toast>"
+                                                      + "<visual>"
+                                                      + "<binding template='ToastText02'>"
+                                                      + "<text id='1'>{0}</text>"
+                                                      + "<text id='2'>{1}</text>"
+                                                      + "</binding>"
+                                                      + "</visual>"
+                                                      + "</toast>";
+
+        private static void Notify(IEnumerable<PullRequest> prs)
+        {
+            if (prs == null)
+                return;
+
+            foreach (var pr in prs)
+            {
+                _speechSynthesizer.Speak($"New Pull Request By {pr.CreatedBy.DisplayName}: {pr.Title}");
+            }
+        }
+
+        private static ToastNotification BuildNotification(PullRequest pr)
+        {
+            var notificationData = string.Format(NotificationTemplate, pr.Title, pr.CreatedBy.DisplayName);
+            var notificationBlob = new XmlDocument();
+            notificationBlob.LoadXml(notificationData);
+            return new ToastNotification(notificationBlob) {Tag = pr.PullRequestId.ToString()};
+        }
+
+        
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+    }
+}
