c8a73cc6800762bbf131157009a22c42bd15c4b8
Avner Shahar-Kashtan <avner@strawjackal.org>
2018-07-29 20:20:16 +0300

    deleting unused files and starting android work

--- PullRequestMonitor.Android/MainActivity.cs
+++ PullRequestMonitor.Android/MainActivity.cs
@@ -1,19 +1,18 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Preferences;
 using Android.Support.V7.App;
 
 namespace PullRequestMonitor.Android
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : PreferenceActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            AddPreferencesFromResource(Resource.);
         }
     }
 }
